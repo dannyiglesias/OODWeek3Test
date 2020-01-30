8cdc25f0ed3b0a460a7767ee91b7309c61dfd164
dannyiglesias <dannyiglesias@gmail.com>
2020-01-30 17:28:47 -0500

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,49 +1,16 @@
 package RaceEngineer;
 
 public class Engine {
-//	boolean on;
-//	int revolutions;
-	
+
 	public Engine() {
 	}
 	
 	public void on() {
 		System.out.println("Engine is on");
-		//on = true;
 	}
 	
 	public void off() {
-		//on = false;
 		System.out.println("Engine is off");
 	}
-	
-//	public void engineOn() {
-//		if (on) {
-//			System.out.println("Engine is on");
-//			
-//		}
-//		
-//	}
-//	
-//	public void engineOff() {
-//		if (on) {
-//			System.out.println("Engine is off");
-//		}
-//	}
-//	
-//	public void setRevolutions(int revolutions) {
-//		this.revolutions = revolutions;
-//	}
-//	
-//	public void warm() {
-//		revolutions = 12000;
-//		System.out.println("Engine is warming up at 12,000 RPM");
-//	
-//	}
-//	
-//	public void idle() {
-//		revolutions = 9000;
-//		System.out.println("Engine is idling a t 9,000 RPM");
-//	}
 
 }
